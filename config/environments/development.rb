a73d97a72131659f6583689725bc47cabc1e343d
hiromurashin <shinshinshu65@seclab.shinshu-u.ac.jp>
2016-05-30 04:44:32 +0000

    Add password resets & email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'rails-tutorial-c9-mhartl.c9.io'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'rails-tutorial-c9-mhartl.c9.io' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,10 +20,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-       post users_path, user: { name:  "Example User",
-                                            email: "user@example.com",
-                                            password:              "password",
-                                            password_confirmation: "password" }
+      post users_path, user: { name:  "Example User",
+                                      email: "user@example.com",
+                                      password:              "password",
+                                      password_confirmation: "password" }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
